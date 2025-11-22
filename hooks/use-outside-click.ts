fc7c9b2b463aa8db3f0d4f3e23387176c5c9265f
kingyousif <95659504+kingyousif@users.noreply.github.com>
2025-11-22 20:57:38 +0300

    Add files via upload

---
+++ hooks/use-outside-click.ts
@@ -0,0 +1,20 @@
+import { useEffect, RefObject } from "react";
+
+export function useOutsideClick(
+  ref: RefObject<HTMLElement | null>,
+  handler: () => void
+) {
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        handler();
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, handler]);
+}
+
